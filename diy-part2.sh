85405a5987b9a29f51b92e92a9c4a7c10e98ac11
YongJunShan <119868381@qq.com>
2021-10-14 16:58:28 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,9 +16,3 @@ sed -i 's/192.168.1.1/192.168.188.254/g' package/base-files/files/bin/config_gen
 sed -i 's/OpenWrt/MKTECH/g' package/base-files/files/bin/config_generate
 # Modify default timezone
 sed -i 's/UTC/CST-8/g' package/base-files/files/bin/config_generate
-# Modify default theme
-sed -i 's/bootstrap/argon/g' feeds/luci/collections/luci/Makefile
-# clone argon theme
-cd package/lean
-rm -rf luci-theme-argon  
-git clone https://github.com/jerrykuku/luci-theme-argon.git  
